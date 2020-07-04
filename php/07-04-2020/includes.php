49c3d704905ea0004444365cfb71caa52b21d15f
devmst <saqib.tariq@zameen.com>
2020-07-04 17:53:07 +0500

    Domain knowlege and Super global array and file includes

---
+++ php/07-04-2020/includes.php
@@ -0,0 +1,18 @@
+<?php
+
+// include
+// include_once
+// require
+// require_once
+
+
+// include_once 'superglobals.php';
+// include_once 'superglobals.php';
+// include_once 'superglobals.php';
+require_once 'superglobals.php';
+
+echo 'After include';
+
+myBook();
+
+
---
+++ php/07-04-2020/superglobals.php
@@ -0,0 +1,17 @@
+<!-- <form action="" method="post">
+    <input type="text" name="fname">
+    <input type="submit" value="Reg">
+</form> -->
+
+<?php
+
+
+echo '<pre>';
+print_r($_SERVER);
+die;
+
+function myBook(){
+    echo 'Book';
+}
+
+    
